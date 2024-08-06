101bc1235f13a068c8247b925ecc411aeb9a8561
Carlos Gonzalez <carlos.custodi@hotmail.com>
2024-08-06 18:21:15 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
-function visibility() {
+function ocultar_texto() {
     document.getElementById("ocultar_imagen").style.display = "none";
     document.getElementById("ocultar_h2").style.display = "none";
 }
 
 function textoGenerado() {
-    let texto = document.getElementById("texto").addEventListener;
-    document.getElementById("cambio_texto").innerText = texto;
+    let texto = document.getElementById("texto");
+    let nuevo_texto = document.getElementById("cambio_texto");
+
+    nuevo_texto.textContent = texto.value;
+    nuevo_texto.style.fontSize = "1.5rem";
+}
+
+function mostrarBoton() {
+    document.getElementById("boton_oculto").style.display = "block";
 }
 
 function encriptar() {
-    visibility();
+    ocultar_texto();
     textoGenerado();
+    mostrarBoton();
 }
 
 function desencriptar() {
-    visibility();
+    ocultar_texto();
     textoGenerado();
+    mostrarBoton();
 }
