e8792ed5c8aeb6baf11c7cfe6d172439852b6d98
Carlos Gonzalez <carlos.custodi@hotmail.com>
2024-08-06 12:08:24 -0500

    Update index.html

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 function visibility() {
     document.getElementById("ocultar_imagen").style.display = "none";
     document.getElementById("ocultar_h2").style.display = "none";
+}
 
-    document.getElementById('cambio_texto').innerHTML = 'Hola soy Carlos';
+function encriptar() {
+    visibility();
+}
+
+function desencriptar() {
+    visibility();
 }
