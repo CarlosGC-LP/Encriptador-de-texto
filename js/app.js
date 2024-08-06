e3fe5a7f2773326ce7249dd8cf11a69612e2da47
Carlos Gonzalez <carlos.custodi@hotmail.com>
2024-08-06 12:14:19 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ function visibility() {
     document.getElementById("ocultar_h2").style.display = "none";
 }
 
+function textoGenerado() {
+    let texto = document.getElementById("texto").innerText;
+    document.getElementById("cambio_texto").innerText = texto;
+}
+
 function encriptar() {
     visibility();
 }
